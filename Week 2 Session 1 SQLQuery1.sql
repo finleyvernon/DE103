f85c5260e7376206bae6a74bf4ed6c7403b9fccd
finleyvernon <fiv0027@arastudent.ac.nz>
2021-08-11 10:57:01 +1200

    Hospital data prior to cleansing - Excel

--- Week 2 Session 1 SQLQuery1.sql
+++ Week 2 Session 1 SQLQuery1.sql
@@ -5,51 +5,12 @@ GO
 CREATE TABLE Artist(
 artistID int primary key,
 firstName varchar(20),
-lastName varchar(20)
+lastName varchar(20),
+email varchar(255),
 );
 delete from [dbo].[Artist];
 
 insert into Artist ( artistID,firstName, lastName, email) values (1, 'Vincent', 'van gogh', 'myemail@email.com'),
 (2, 'Vincent', 'Cassel','donotknow@email.com'), (3, 'Mike', 'Lance', 'clever@email.com');
 
-
- 
-
--- Test
-select * from Artist;
-
-select [firstName], [lastName]
-from [dbo].[Artist];
-
-CREATE TABLE Painting(
-paintingID int primary key,
-paintingName varchar(25),
-artistID int,
-foreign key (artistID) references Artist (artistID)
-);
-insert into Painting (paintingID,paintingName,artistID)   values(1, 'Sunflower', 1),
-(2, 'UML', 3);
-
-select * from [dbo].[Painting];
--- interesting + Aliasing + DRY
-select [firstName] as 'First Name', [lastName] as 'Last Name', [paintingName] as
-'Name of the Painting'
-from [dbo].[Artist] A , [dbo].[Painting] P
-where A.[artistID] = P.[artistID];
-
--- Deleting table + data
-drop table Painting;
--- Delete data from table but not the table itself
--- delete from [dbo].[Painting];
-select * from [dbo].[Painting];
--- Alternate option called ALTER
-ALTER TABLE Artist
-ADD Email varchar(255);
-
--- Edit stuff also
-Update Artist
-set firstName = 'Rob'
-where  artistID = 1;
-
-
-
+SELECT * FROM Artist;
